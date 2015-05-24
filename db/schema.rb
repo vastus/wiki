fb37ad8d70174ec143326bb6f62647f46aa19cc0
Juho Hautala <juho.hautala@helsinki.fi>
2015-05-24 19:45:51 +0300

    add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522101140) do
+ActiveRecord::Schema.define(version: 20150524130731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150522101140) do
     t.string   "nickname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "admin"
   end
 
   add_foreign_key "pages", "users"
