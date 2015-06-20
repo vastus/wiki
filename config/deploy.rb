c938244b775b023d4122f1c4a221bca0658709af
Juho Hautala <juho.hautala@helsinki.fi>
2015-06-20 13:06:54 +0300

    change puma conf val to point to shared path

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,12 +10,12 @@ set(:deploy_to, "/apps/#{fetch(:application)}")
 # Default value for :pty is false
 # set :pty, true
 
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set(:linked_files, fetch(:linked_files, []).push(["config/env.yml"]))
-
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set(:linked_files, fetch(:linked_files, []).push("config/env.yml"))
 
 # Default value for default_env is {}
 set(:default_env, {:SECRET_KEY_BASE => "reijoernoseija"})
@@ -28,7 +28,7 @@ set(:rbenv_map_bins, %w(rake gem bundle ruby rails))
 set(:rbenv_roles, :all) # default value
 
 # puma
-set(:puma_conf, nil)
+set(:puma_conf, "#{shared_path}/config/puma.rb")
 set(:puma_state, "/tmp/puma.state")
 set(:puma_pid, "/tmp/puma.pid")
 set(:puma_default_control_app, "unix:///tmp/puma.wiki.sock")
